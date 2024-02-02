3867d575879fa2b31c5b47041cb930584708efa2
julijajuskeviciene <julija.juskeviciene@gmail.com>
2024-02-02 12:03:39 +0200

    Modified class AccountRegistrationPage

--- src/test/java/lt/julija/page/mohito/AccountRegistrationPage.java
+++ src/test/java/lt/julija/page/mohito/AccountRegistrationPage.java
@@ -7,7 +7,7 @@ import lt.julija.page.PageBase;
 public class AccountRegistrationPage extends PageBase {
 
     public static void clickOnButtonAcceptCookies() {
-        Common.waitElementVisible(Locator.Mohito.AccountRegistration.buttonAcceptCookies,6);
+        Common.waitElementVisible(Locator.Mohito.AccountRegistration.buttonAcceptCookies, 6);
         Common.clickOnElement(Locator.Mohito.AccountRegistration.buttonAcceptCookies);
     }
 
@@ -17,19 +17,19 @@ public class AccountRegistrationPage extends PageBase {
 
     public static void enterEmail(String email) {
         Common.waitElementVisible(Locator.Mohito.AccountRegistration.inputEmail, 8);
-        Common.sendKeysToElement(Locator.Mohito.AccountRegistration.inputEmail,email);
+        Common.sendKeysToElement(Locator.Mohito.AccountRegistration.inputEmail, email);
     }
 
     public static void enterName(String name) {
-        Common.sendKeysToElement(Locator.Mohito.AccountRegistration.inputName,name);
+        Common.sendKeysToElement(Locator.Mohito.AccountRegistration.inputName, name);
     }
 
     public static void enterLastName(String lastName) {
-        Common.sendKeysToElement(Locator.Mohito.AccountRegistration.inputLastName,lastName);
+        Common.sendKeysToElement(Locator.Mohito.AccountRegistration.inputLastName, lastName);
     }
 
     public static void enterPassword(String password) {
-        Common.sendKeysToElement(Locator.Mohito.AccountRegistration.inputPassword,password);
+        Common.sendKeysToElement(Locator.Mohito.AccountRegistration.inputPassword, password);
     }
 
     public static void clickOnButtonCreateAccount() {
