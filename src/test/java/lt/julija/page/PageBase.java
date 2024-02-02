6cffdedc6d41a3764d53807fa38b64bf51c3115b
julijajuskeviciene <julija.juskeviciene@gmail.com>
2024-02-02 12:05:39 +0200

    Inherit PageBase class

--- src/test/java/lt/julija/page/PageBase.java
+++ src/test/java/lt/julija/page/PageBase.java
@@ -1,7 +1,7 @@
 package lt.julija.page;
 
 public class PageBase {
-    public static void open(String url, int seconds){
+    public static void open(String url, int seconds) {
         Common.setUpDriver(seconds);
         Common.openUrl(url);
     }
